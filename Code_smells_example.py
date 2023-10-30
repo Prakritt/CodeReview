acb88744248ecc14b23ad25aca6ac74aa899fc25
Prakrit Uprety <prakritupreety0@gmail.com>
2023-10-30 11:22:31 -0700

    Modify: Refactor All Files

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -2,9 +2,11 @@
 def calculate_total_price(items):
     total = 0
     for item in items:
-        if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+        total = total +  calculate_item_price(item)
     return total
+
+def calculate_item_price(item):
+    if item.quantity > 0:
+        if item.price > 100:
+            return item.quantity * item.price
+    return 0
